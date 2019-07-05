4bf66ea28d44f674a3ed9227acaf03c7880cd34f
LanaDz <svitlana.dzyuban@gmail.com>
2019-07-05 14:55:17 +0800

    Move logic to Sorter class, add Sortable interface

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,7 @@
-class Sorter {
-  constructor(public collection: number[]) {}
-  sort(): void {
-    const { length } = this.collection;
-
-    for (let i = 0; i < length; i++) {
-      for (let j = 0; j < length - i - 1; j++) {
-        if (this.collection[j] > this.collection[j + 1]) {
-          const leftHand = this.collection[j];
-          this.collection[j] = this.collection[j + 1];
-          this.collection[j + 1] = leftHand;
-        }
-      }
-    }
-  }
-}
-
-const sorter = new Sorter([10, -1, 20, 21, 12]);
+import { Sorter } from './Sorter';
+import { NumbersCollection } from './NumbersCollection';
+const numbersCollection = new NumbersCollection([0, -5, -1, 120, 21, 12]);
+const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(sorter.collection);
+console.log(numbersCollection.data);
