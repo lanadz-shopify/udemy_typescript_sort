224b21412735efbdf94509ddd88d63c1721b5914
LanaDz <svitlana.dzyuban@gmail.com>
2019-07-13 20:40:44 +0800

    Linkedlist

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 const numbersCollection = new NumbersCollection([0, -5, -1, 120, 21, 12]);
 
 let sorter = new Sorter(numbersCollection);
@@ -11,3 +12,13 @@ const charsCollection = new CharactersCollection('aasdwrgijashdAUIF');
 sorter = new Sorter(charsCollection);
 sorter.sort();
 console.log(charsCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-500);
+linkedList.add(1500);
+linkedList.add(500);
+
+sorter = new Sorter(linkedList);
+sorter.sort();
+linkedList.print();
